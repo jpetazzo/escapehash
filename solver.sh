f88d06de71e55e7151fc36b93e9f16bd8f3f457b
AJ Bowen <aj@soulshake.net>
2021-10-24 20:46:47 -0500

    Use bash and quote variables

--- solver.sh
+++ solver.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 [ "$TEAM" ] || {
     echo "TEAM environment variable not set; using default value 'rainbow'."
@@ -15,12 +15,12 @@ set -eu
 solve_hash() {
     HASH=$1
     DIFFICULTY=$2
-    LOW=$((10**($DIFFICULTY-1)))
-    HIGH=$((10**$DIFFICULTY))
+    LOW=$((10 ** (DIFFICULTY - 1)))
+    HIGH=$((10 ** DIFFICULTY))
     for I in $(seq $LOW $HIGH); do
         TRYHASH=$(echo "$TEAM.$I" | sha256sum | awk '{print $1}')
         if [ "$TRYHASH" = "$HASH" ]; then
-            echo $I
+            echo "$I"
             return
         fi
     done
@@ -32,20 +32,20 @@ solve_challenge() {
     CHALLENGE=$1
     DIFFICULTY=$2
     SOLUTION=""
-    for HASH in $(curl -fs $SERVER/v1/team/$TEAM/challenge/$CHALLENGE.txt); do
-        SOLUTION=$SOLUTION$(solve_hash $HASH $DIFFICULTY)
+    for HASH in $(curl -fs "$SERVER/v1/team/$TEAM/challenge/$CHALLENGE.txt"); do
+        SOLUTION=$SOLUTION$(solve_hash "$HASH" "$DIFFICULTY")
     done
-    echo $SOLUTION
+    echo "$SOLUTION"
 }
 
 solve_all() {
     curl -fs localhost:5000/v1/challenges \
-    | jq -r '.[] | [ .name, .difficulty ] | @tsv' \
-    | while read CHALLENGE DIFFICULTY ; do
-        echo "Solving challenge $CHALLENGE with difficulty $DIFFICULTY."
-        SOLUTION=$(solve_challenge $CHALLENGE $DIFFICULTY)
-        curl -fs $SERVER/v1/team/$TEAM/challenge/$CHALLENGE -H content-type:text/plain --data $SOLUTION
-    done
+        | jq -r '.[] | [ .name, .difficulty ] | @tsv' \
+        | while read -r CHALLENGE DIFFICULTY; do
+            echo "Solving challenge $CHALLENGE with difficulty $DIFFICULTY."
+            SOLUTION=$(solve_challenge "$CHALLENGE" "$DIFFICULTY")
+            curl -fs "$SERVER/v1/team/$TEAM/challenge/$CHALLENGE" -H content-type:text/plain --data "$SOLUTION"
+        done
 }
 
 solve_all
